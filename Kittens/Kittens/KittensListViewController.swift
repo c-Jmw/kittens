b23c2395fa686bd6fc8154e583fe36f6922e33b2
Casey <casey.j.wilkinson@gmail.com>
2015-06-02 20:18:43 +1000

    Add viewController file
    Empty viewController file ready for config

---
+++ Kittens/Kittens/KittensListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  KittensListViewController.swift
+//  Kittens
+//
+//  Created by casey macrae on 2/06/2015.
+//  Copyright (c) 2015 casey macrae. All rights reserved.
+//
+
+import Cocoa
+
+class KittensListViewController: UIViewController {
+
+}
